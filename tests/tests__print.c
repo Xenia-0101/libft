cc09e1df48c6aeb0c88ad8a8fe94b5d279c4f9e5
xenia <xeniavislocka@gmail.com>
2024-04-11 21:26:56 +0200

    Color format print statements

--- tests/tests__print.c
+++ tests/tests__print.c
@@ -3,7 +3,7 @@
 
 void tests__print(const char *test, int passed) {
 	if (passed)
-		write(1,"\x1b[1;32mOK:\t\e", 13);
+		write(1,"\x1b[1;32mOK:\t", 12);
 	else
 		write(1,"\x1b[1;31mFAILED:\t", 16);
 	printf("%s\n", test);
