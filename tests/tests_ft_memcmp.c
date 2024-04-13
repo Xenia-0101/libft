c9309e32b76e2b54ebc3763f5b4a7f4e15f013d6
xenia <xeniavislocka@gmail.com>
2024-04-13 22:33:53 +0200

    Fix tests for ft_memcmp

--- tests/tests_ft_memcmp.c
+++ tests/tests_ft_memcmp.c
@@ -15,76 +15,78 @@
 
 void test_ft_memcmp_1_equal(void)
 {
-	int res;
-	int passed;
+	int passed = 1;
+	const void *s1 = "hello";
+	const void *s2 = "hell0";
+	int res = ft_memcmp(s1, s2, 5);
+	int t_res = memcmp(s1, s2, 5);
 
-	res = ft_memcmp("hello", "hello", 5);
-	passed = 1;
-	if (res != memcmp("hello", "hello", 5))
+	if (res != t_res)
 		passed = 0;
 	tests__print("test_ft_memcmp_1_equal", passed);
 }
 
 void test_ft_memcmp_2_larger(void)
 {
-	int res;
-	int passed;
+	int passed = 1;
+	const void *s1 = "hello";
+	const void *s2 = "hell";
 
-	res = ft_memcmp("hello", "hell", 5);
+	int res = ft_memcmp(s1, s2, 5);
+	int t_res = memcmp(s1, s2, 5);
 	passed = 1;
-	if (res != memcmp("hello", "hell", 5))
+	if (res != t_res)
 		passed = 0;
 	tests__print("test_ft_memcmp_2_larger", passed);
 }
 
 void test_ft_memcmp_3_lower(void)
 {
-	int res;
-	int passed;
+	int passed = 1;
+	const void *s1 = "hell";
+	const void *s2 = "hell0";
 
-	res = ft_memcmp("hell", "hello", 5);
+	int res = ft_memcmp(s1, s2, 5);
+	int t_res = memcmp(s1, s2, 5);
 	passed = 1;
-	if (res != memcmp("hell", "hello", 5))
+	if (res != t_res)
 		passed = 0;
 	tests__print("test_ft_memcmp_3_lower", passed);
 }
 
 void test_ft_memcmp_4_empty_1(void)
 {
-	int res;
-	int passed;
+	int passed = 1;
+	const void *s1 = "";
+	const void *s2 = "hell0";
 
-	res = ft_memcmp("", "hello", 5);
+	int res = ft_memcmp(s1, s2, 5);
+	int t_res = memcmp(s1, s2, 5);
 	passed = 1;
-	if (res != memcmp("", "hello", 5))
+	if (res != t_res)
 		passed = 0;
 	tests__print("test_ft_memcmp_4_empty_1", passed);
 }
 
 void test_ft_memcmp_5_empty_2(void)
 {
-	int res;
-	int passed;
+	int passed = 1;
+	const void *s1 = "hello";
+	const void *s2 = "";
 
-	res = ft_memcmp("hello", "", 5);
+	int res = ft_memcmp(s1, s2, 5);
+	int t_res = memcmp(s1, s2, 5);
 	passed = 1;
-	if (res != memcmp("hello", "", 5))
+	if (res != t_res)
 		passed = 0;
 	tests__print("test_ft_memcmp_5_empty_2", passed);
 }
 
 void	tests_ft_memcmp(void)
 {
-
 	test_ft_memcmp_1_equal();
 	test_ft_memcmp_2_larger();
 	test_ft_memcmp_3_lower();
 	test_ft_memcmp_4_empty_1();
 	test_ft_memcmp_5_empty_2();
-	printf("%d\n", memcmp("hello","hell",  5));
-	printf("%d\n", memcmp("hell","helo",  5));
-	printf("%d\n", memcmp("hel","hello",  5));
-	printf("%d\n", memcmp("he","hello",  5));
-	printf("%d\n", memcmp("h","hello",  5));
-	printf("%d\n", memcmp("","hello",  5));
 }
