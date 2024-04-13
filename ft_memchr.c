25140dd21eb52ee4de91e21ea677faa3d584ec3b
xenia <xeniavislocka@gmail.com>
2024-04-13 17:11:15 +0200

    Add ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xvislock <xvislock@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/13 16:41:06 by xvislock          #+#    #+#             */
+/*   Updated: 2024/04/13 16:41:06 by xvislock         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+DESCRIPTION
+       The  memchr()  function scans the initial n bytes of the memory area pointed to by s for the first instance of
+       c.  Both c and the bytes of the memory area pointed to by s are interpreted as unsigned char.
+
+RETURN VALUE
+       The  memchr() function returns a pointer to the matching byte or NULL if the character does not
+       occur in the given memory area.
+*/
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	while (*s && n++ > 0)
+	{
+		if (*s == c)
+			return (s);
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -29,5 +29,6 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strrchr(const char *s, int c);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
