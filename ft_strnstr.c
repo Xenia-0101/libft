ce7e47a73add437f71e9f907a3884d664e7eed6d
xenia <xeniavislocka@gmail.com>
2024-04-14 15:01:33 +0200

    Add tests for ft_strcmp

---
+++ ft_strnstr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xvislock <xvislock@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/13 22:34:59 by xvislock          #+#    #+#             */
+/*   Updated: 2024/04/13 22:34:59 by xvislock         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	The strnstr() function locates the first occurrence of the  null-termi-
+	nated  string little in the string big, where not more than len charac-
+	ters are searched.  Characters that appear after a `\0' character  are
+	not  searched.
+*/
+char	*ft_strnstr(const char *big, const char *little, size_t n)
+{
+	const char *b;
+	const char *l;
+
+	if (!*little)
+		return ((char *)big);
+	l = little;
+	while (*big && n-- > 0)
+	{
+		b = big;
+		if (*big == *l)
+		{
+			while (*big++ == *l++)
+			{
+				if (!*l)
+					return ((char *)b);
+				if (n-- == 0)
+					return (NULL);
+			}
+			l = little;
+		}
+		else
+			big++;
+	}
+	return (NULL);
+}
